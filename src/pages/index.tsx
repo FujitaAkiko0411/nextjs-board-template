0fad0ff7b9f6f4d3333e166f985180a66ec7bb1b
Akiko <fujita.akiko22@shizuoka.ac.jp>
2023-06-08 16:49:02 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello, world!</p>
+        <p>ああああ</p>
       </main>
     </>
   );
